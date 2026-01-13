9b29556c8fe2ab5effe6d1c4c88a2d0b97a2223f
florian <florian@mbp-de-florian.home>
2026-01-13 15:06:26 +0100

    define basic model for real estate advertisement, implement code formatter ruff, setting basic config

--- meilleurecopro/stats/models.py
+++ meilleurecopro/stats/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    """
+    Represents a real estate advertisement
+    """
+
+    bienici_id = models.CharField(null=True, max_length=255)
+    condominium_fees = models.FloatField(null=False)
+    department = models.CharField(default=None)
+    city = models.CharField(null=False)
+    zipcode = models.CharField(null=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.department:
+            self.department = self.zipcode[0:2]
